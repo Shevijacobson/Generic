8c20bb41bac6ee99fc2661dbe1423d14e92f7cd3
Shevijacobson <shevijacob@gmail.com>
2024-06-10 00:03:51 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+      
+        SwapList<string> names = new SwapList<string> { "shevi", "batshi", "chani", "dina" };
+       
+        names.Swap(1,2);
+        Console.WriteLine(string.Join(", ", names));
+
+
+    }
+}
---
+++ ConsoleApp1/SwapList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SwapList<T> : List<T>
+    {
+
+        public void Swap(int index1, int index2)
+        {
+
+            T item = this[index1];
+            this[index1] = this[index2];
+            this[index2] = item;
+        }
+    }
+}
